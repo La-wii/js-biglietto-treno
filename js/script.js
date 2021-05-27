eb6f387de335a3317f9c2844358bdc1844150b1a
La-wii <sentilavale@gmail.com>
2021-05-27 16:37:35 +0200

    .tofixed

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
 var km = parseInt(prompt('inserisci i km che vuoi percorrere'));
 var eta = parseInt(prompt('inserisci la tua età'));
-
+var minorenne;
+var over65;
 var biglietto = 0.21 * km;
+
 
 // var minorenne = biglietto * 80 / 100;
 // console.log(minorenne);
 
 // test 2
-var minorenne = biglietto - biglietto * 20 / 100;
-console.log(minorenne);
+// var minorenne = biglietto - biglietto * 20 / 100;
+// console.log(minorenne);
 
+
+if (eta <= 17) {
+    biglietto = biglietto - biglietto * 20 / 100;
+}
+
+else if (eta >= 65) {
+    biglietto = biglietto - biglietto * 40 / 100;
+    
+}
+
+document.getElementById("prezzo").innerHTML = biglietto.toFixed(2); 
 
 
 
